4a27245216824a2310949dad8de3b863062db140
Jhon <c2404276@trbvn.com>
2017-12-15 16:57:32 +0100

    0.3

---
+++ 03_curent_date.js
@@ -0,0 +1,18 @@
+let show = document.getElementById('data');
+let calendar = new Date();
+let data = {
+ spacer: function() { //losowo wyświetla / albo - między cyframi korzystając z właściwości rand (wiersz:89)
+   if ( this.rand === 0) {
+    return "/";
+   } else {
+    return "-";
+   }
+ },
+ rand: Math.floor(Math.random() * 100 / 10) % 2,
+ day: calendar.getDate(),
+ month: calendar.getMonth() + 1,
+ year: calendar.getFullYear()
+
+};
+
+show.innerHTML = "Dzisiaj jest: " + data.day + data.spacer() + data.month + data.spacer() + data.year;
